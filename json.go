8621bc6b0605ef306ad24c097127f43cfd971495
Akorede <akoredeadewole8@gmail.com>
2024-08-26 17:04:49 +0100

    added success and error response

--- json.go
+++ json.go
@@ -19,3 +19,17 @@ func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
 	w.WriteHeader(code)
 	w.Write(data)
 }
+
+func respondwithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Println("Responding with 5XX error: ", msg)
+	}
+
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJson(w, code, errorResponse{
+		Error: msg,
+	})
+}
