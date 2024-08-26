5cb47d5074169316dc4422ee16de5b5f589262cd
Akorede <akoredeadewole8@gmail.com>
2024-08-26 16:46:41 +0100

    started with building apis with Go

--- main.go
+++ main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello world")
-
 	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
@@ -20,4 +21,27 @@ func main() {
 	}
 
 	fmt.Println("Port: ", portString)
+
+	router := chi.NewRouter()
+
+	cors := cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"Authorization", "Content-Type"},
+		AllowCredentials: true,
+	})
+
+	router.Use(cors)
+
+	v1Router := chi.NewRouter()
+	v1Router.HandleFunc("/healthz", handlerReadiness)
+
+	router.Mount("/v1", v1Router)
+
+	log.Printf("Server started on port %v", portString)
+	err := http.ListenAndServe(":8000", router)
+	if err != nil {
+		log.Fatal("Error starting server:", err)
+	}
+
 }
